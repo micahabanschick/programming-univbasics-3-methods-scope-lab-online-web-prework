e576f485f15782f1b155135b1c63d6b93f02abca
Micah Banschick <micha.ban@gmail.com>
2020-01-13 00:05:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,11 @@
 def mario
-  puts phrase
+  puts 'It\'s-a me, Mario!'
 end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  puts 
